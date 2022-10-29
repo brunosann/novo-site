fe86e5951b351306694d6797e7528a7ad56f99e3
Bruno Teixeira <bruno_teixeira20@hotmail.com>
2022-10-29 12:33:50 -0300

    feat(backend-route): creating route not found

--- backend/app/routes/api.php
+++ backend/app/routes/api.php
@@ -1,7 +1,15 @@
 <?php
 
 use Pecee\SimpleRouter\SimpleRouter as Route;
+use Pecee\Http\Request;
 
-Route::get('/', function () {
-  return 'Hello world';
+Route::get('/not-found', function () {
+  response()->httpCode(404)->json(['message' => 'page not found']);
+});
+
+Route::error(function (Request $request, \Exception $exception) {
+  switch ($exception->getCode()) {
+    case 404:
+      response()->redirect('/not-found');
+  }
 });

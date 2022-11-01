20d52a9af60b4cdbf87e17a93b01894f0b07e061
Bruno Teixeira <bruno_teixeira20@hotmail.com>
2022-11-01 09:21:39 -0300

    feat(backend-email): installing PHPMailer and creating email class

---
+++ backend/app/classes/Mail.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace app\classes;
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+class Mail
+{
+  public PHPMailer $mail;
+
+  function __construct()
+  {
+    $this->mail = new PHPMailer(true);
+
+    $this->config();
+  }
+
+  public function config()
+  {
+    $this->mail->SMTPDebug = SMTP::DEBUG_SERVER;
+    $this->mail->isSMTP();
+    $this->mail->Host = $_ENV['MAIL_HOST'];
+    $this->mail->SMTPAuth = true;
+    $this->mail->Username = $_ENV['MAIL_USERNAME'];
+    $this->mail->Password = $_ENV['MAIL_PASSWORD'];
+    // $this->mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+    $this->mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $this->mail->Port = $_ENV['MAIL_PORT'];
+
+    $this->mail->setFrom($_ENV['MAIL_FROM_ADDRESS'], $_ENV['MAIL_FROM_NAME']);
+    $this->mail->addAddress($_ENV['MAIL_TO_ADDRESS'], $_ENV['MAIL_TO_NAME']); //Add a recipient
+
+    $this->mail->isHTML(true);
+    $this->mail->Subject = 'Dev Teixeira Contato';
+  }
+
+  public function body($content)
+  {
+    $this->mail->Body = $content;
+  }
+
+  public function send()
+  {
+    try {
+      $this->mail->send();
+    } catch (Exception $e) {
+      echo "Message could not be sent. Mailer Error: {$this->mail->ErrorInfo}";
+    }
+  }
+}

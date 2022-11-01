b0753b50be4caa68724819d4a40a6b284470d48b
Bruno Teixeira <bruno_teixeira20@hotmail.com>
2022-11-01 09:23:15 -0300

    feat(backend-contact): sending email and marking contact as sent in the database

--- backend/app/controllers/ContactController.php
+++ backend/app/controllers/ContactController.php
@@ -2,6 +2,8 @@
 
 namespace app\controllers;
 
+use app\classes\Mail;
+use app\classes\View;
 use app\models\Contact;
 
 class ContactController
@@ -10,6 +12,17 @@ class ContactController
   {
     $contactModel =  new Contact;
     $contact = $contactModel->insert(input()->all());
+
+    try {
+      $mail = new Mail;
+      $mail->body(View::render('contact', $contact));
+      $mail->send();
+
+      $contactModel->markAsSent($contact['id']);
+    } catch (\Throwable $th) {
+      $contact['error'] = $th->getMessage();
+    }
+
     response()->json($contact);
   }
 }

7e41e4e4358702422fdb3d17d9bc0f347d524a82
Bruno Teixeira <bruno_teixeira20@hotmail.com>
2022-11-02 14:35:20 -0300

    feat(frontend-contact): sending request to send email

--- src/components/SectionContact/index.jsx
+++ src/components/SectionContact/index.jsx
@@ -1,13 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 import styles from "./styles.module.scss";
 import imgEnvelope from "../../assets/images/envelope.png";
+import useFetch from "../../hooks/useFetch";
 
 export const SectionContact = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [subject, setSubject] = useState("");
   const [message, setMessage] = useState("");
+  const [errorSubmit, setErrorSubmit] = useState(false);
+
+  const { request, error, data, loading } = useFetch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = { name, email, subject, message };
+
+    const isUnfilledData = Object.entries(data).some((item) => !item[1].trim());
+
+    if (isUnfilledData) return setErrorSubmit(true);
+    else setErrorSubmit(false);
+
+    request("https://api.devteixeira.com.br/contact", {
+      method: "POST",
+      body: JSON.stringify(data),
+    });
+  };
+
+  useEffect(() => {
+    setName("");
+    setEmail("");
+    setSubject("");
+    setMessage("");
+  }, [data]);
 
   return (
     <section id="contact" className={styles.sectionContact}>
@@ -15,7 +41,7 @@ export const SectionContact = () => {
         <h2 className={styles.title}>Vamos Conversar</h2>
 
         <div className={styles.content}>
-          <form className={styles.border}>
+          <form onSubmit={handleSubmit} className={styles.border}>
             <h3>Contato</h3>
             <div className={styles.formBox}>
               <input
@@ -52,7 +78,7 @@ export const SectionContact = () => {
             </div>
             <div className={styles.formBox}>
               <textarea
-                className={styles.filled}
+                required
                 rows="5"
                 placeholder="Sua mensagem"
                 className={message && styles.filled}
@@ -61,7 +87,13 @@ export const SectionContact = () => {
               ></textarea>
               <span>Mensagem</span>
             </div>
-            <button className="btn-primary">Enviar</button>
+            {errorSubmit && (
+              <p className={styles.errorSubmit}>Preencha todos os campos</p>
+            )}
+            {data && !errorSubmit && <p>Mensagem enviada </p>}
+            <button type="submit" className="btn-primary" disabled={loading}>
+              {loading ? "Enviando..." : "Enviar"}
+            </button>
           </form>
           <div className={styles.socials}>
             <a
